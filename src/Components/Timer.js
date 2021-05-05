dc0950ceb50570dce2e7107bef774cc015ce9c53
Grizzly <b.mullerjnr@gmail.com>
2021-05-05 22:31:20 +0200

    'pomodoro-initial'

---
+++ src/Components/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/Timer.css'
+
+function Timer() {
+    return (
+        <div className="timer_Container">'
+            <div className="timer_Face">
+                <h1>00:00</h1>
+            </div>            
+        </div>
+    )
+}
+
+export default Timer
