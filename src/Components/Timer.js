62e3e4ab32724914045d676b29c9307e029bed43
Grizzly <b.mullerjnr@gmail.com>
2021-05-08 00:06:31 +0200

    'basic-pomodoro'

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,22 +2,45 @@ import React, {useState, useEffect} from 'react'
 import '../Styles/Timer.css'
 
 function Timer() {   
-    const [minutes, setMinutes] = useState(25);
-    const [seconds, setSeconds] = useState(0); 
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(5); 
     const [displayMesage, setDisplayMesage] = useState(false);
+    
 
-    useEffect(()=>{}, [seconds])
+    useEffect(()=>{
+        let interval = setInterval(()=>{
+            clearInterval(interval);
+
+            if(seconds === 0) {
+                if(minutes !== 0) {
+                    setSeconds(59);
+                    setMinutes(minutes - 1)
+                } else {
+                    let minutes = displayMesage? 24 : 4;
+                    let seconds = 59;
+
+                    setMinutes(minutes);
+                    setSeconds(seconds);
+                    setDisplayMesage(!displayMesage)
+                }
+            } else { 
+                setSeconds( seconds - 1);
+            }
+        }, 1000)
+    }, [seconds])
+
+    const timerMinutes = minutes < 10? `0${minutes}`: minutes;
+    const timerSeconds = seconds < 10? `0${seconds}`: seconds;
 
 
     return (
         <div className="timer_Container">
-            <div className="message">
+            {displayMesage? <div className="message">
                 <h2>Break</h2>
-            </div>
+            </div> : null
+            }
             <div className="timer_Face">
-                <h2 className="time-minute">{minutes}</h2>
-                <h2>:</h2>
-                <h2 className="time-second">{seconds}</h2>
+                {timerMinutes}:{timerSeconds}
             </div>         
         </div>
     )
